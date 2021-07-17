84794548f4ded7440363d08bf99156b20283d89f
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-17 17:07:25 +0300

    creates methods to save, delete and update profile

--- rate/models.py
+++ rate/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
+import cloudinary
+from cloudinary.models import CloudinaryField
+
+
 # Create your models here.
 class Profile(models.Model):
     '''
     class to create user profile
     '''
-    profile_photo = models.ImageField(upload_to = 'pictures')
+    profile_photo = CloudinaryField('image', blank=True, null=True)
     bio = models.TextField(max_length=100)
     user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+
+    def __str__(self):
+        
+        return self.profile_photo.url
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+    
+    @classmethod   
+    def update_bio(cls,id,new_bio):
+        cls.objects.filter(pk = id).update(bio=new_bio)
+        new_bio_object = cls.objects.get(bio = new_bio)
+        new_bio = new_bio_object.bio
+        return new_bio
