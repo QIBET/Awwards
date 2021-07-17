c0e942305c592a9e92f683d67733ad05ea254436
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-17 13:11:34 +0300

    creates user registration function

--- rate/views.py
+++ rate/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def index(request):
     
     return render(request, 'index.html')
+
+def register(request):
+	if request.user.is_authenticated:
+		return redirect('index')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('login')
+			
+
+		context = {'form':form}
+		return render(request, 'registration/register.html', context)
+
