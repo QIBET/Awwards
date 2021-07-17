a4aeaf2cd83cc5dcbeb620b29ce7424b2142799a
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-17 16:58:37 +0300

    creates class to create userprofile

--- rate/models.py
+++ rate/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    '''
+    class to create user profile
+    '''
+    profile_photo = models.ImageField(upload_to = 'pictures')
+    bio = models.TextField(max_length=100)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
